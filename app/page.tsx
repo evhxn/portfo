a37e939a2a51f5810a4a73ffe9dd067b0e69b5ee
evhxn <72643137+evhxn@users.noreply.github.com>
2025-02-26 18:15:08 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { BlogPosts } from 'app/components/posts'
+aimport { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
@@ -7,12 +7,16 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`Hello, I'm a Computer Science student at Chapman University. 
-        My passion is blending creativity and technology to design unique 
-        solutions. I thrive in environments where synergy meets innovation, 
-        especially when tackling challenges that require thinking outside the box. 
-        I love experimenting with new ideas and building things that merge the practical and the imaginative.
-        `}
+        {`hello, i'm ethan tapia. im currently pursuing a b.s in computer science at chapman university, 
+        as well as a m.s in electrical engineering & computer science starting fall 2026. my passion
+        is making things, whether it's software, cad or show programming. i love  design unique 
+        solutions that blend creativity and technology. i'm interested in turning imaginative
+        ideas into practical projects that can inspire and empower others. i believe in
+        collaboration and enjoy learning from every challenge i face, whether it's
+        debugging robotics code or refining a 3d model. im currently seeking any
+        role that involves creative engineering, where i can combine technical
+        skills with my passion for innovation.
+                `}
       </p>
       <div className="my-8">
         <BlogPosts />
